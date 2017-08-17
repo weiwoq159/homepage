3c5bc928b01a6ae56811fe79cfed2f9cfbc69948
maqiang <529524114@qq.com>
2017-08-17 11:17:06 +0800

    adsf

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
 /*
  * @Author: 马强 
  * @Date: 2017-08-15 14:25:22 
- * @Last Modified by:   jerry 
- * @Last Modified time: 2017-08-15 14:25:22 
+ * @Last Modified by: jerry
+ * @Last Modified time: 2017-08-16 18:02:34
  */
-
+// 轮播图
 var arry = [];
 var num = 0;
 $(document).ready(function () {
@@ -12,7 +12,7 @@ $(document).ready(function () {
         arry.push($(this))
     })
     var int = setInterval(function(){
-        if(num == arry.length-1){
+        if(num >= arry.length-1){
             $('.app_banner_child ul').animate({left:'0'},2000)
             num = 0
         }
@@ -23,3 +23,20 @@ $(document).ready(function () {
        
     },8000)
 });
+
+// 验证码倒计时按钮
+var wait = 60
+function time(event){
+    var _this = $('#sub')
+    _this.removeAttr('onclick').attr('disabled','disabled')
+    var timeout = setInterval(function(){
+        _this.html(wait+'s')
+        wait--;
+        if(wait<0){
+            _this.html('获取验证码');
+            _this.removeAttr('disabled').attr('onclick','time(event)')
+            clearInterval(timeout)
+            wait = 60
+        }
+    },1000)
+}
