092fa884ede9ddbec6ef3b1d9eb3d9b86e30bb47
maqiang <529524114@qq.com>
2017-08-21 14:41:41 +0800

    2017-08-21

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,27 @@
 /*
  * @Author: 马强 
  * @Date: 2017-08-15 14:25:22 
- * @Last Modified by: jerry
- * @Last Modified time: 2017-08-16 18:02:34
+ * @Last Modified by: 马强
+ * @Last Modified time: 2017-08-21 13:04:10
  */
 // 轮播图
 var arry = [];
-var num = 0;
+var num = 1;
 $(document).ready(function () {
+    var Wwidth = $(window).width()
     $('.app_banner_child ul li').each(function(){
         arry.push($(this))
     })
+    console.log(arry)
     var int = setInterval(function(){
-        if(num >= arry.length-1){
+        if(num >= arry.length){
             $('.app_banner_child ul').animate({left:'0'},2000)
-            num = 0
+            num = 1
         }
         else{
-            $('.app_banner_child ul').animate({left:(num+1)*-1920},2000)
-            num++
+             $('.app_banner_child ul').animate({left:num * -1900},2000)
+             num++
+            
         }
        
     },8000)
@@ -40,3 +43,20 @@ function time(event){
         }
     },1000)
 }
+// question and anwser
+var b = true;
+$('.FandA_box').on('click', function () {
+
+    var display = $(this).find('.anwser').css('display')
+    
+    if(display === 'none'){
+        $(this).find('.anwser').show();
+        $(this).find('.question img').hide();
+        $(this).find('.top').show();
+    }
+    else{
+        $(this).find('.anwser').hide();
+        $(this).find('.question img').show();
+        $(this).find('.top').hide();
+    }
+});
